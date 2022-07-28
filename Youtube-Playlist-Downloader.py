4286963c046b498713e5ec6ed63e45f9882c99a0
Foxtrot-813 <84441705+Foxtrot-813@users.noreply.github.com>
2022-07-28 18:44:56 +0200

    Update Youtube-Playlist-Downloader.py

--- Youtube-Playlist-Downloader.py
+++ Youtube-Playlist-Downloader.py
@@ -1,32 +1,60 @@
 import os
 import re
-from pytube import Playlist
+from pytube import Playlist, YouTube
 
-url = input("Playlist link:\n")
-choice = int(input("Please choose one of the options:\n1-Audio\n2-Video\n3-Quit\n"))
-playlist = Playlist(url)
-if choice == 3:
+opt = int(input("Please choose an option:\n1-Download a playlist.\n2-Download only one video.\n3-Exit\n> "))
+choice = int(input("Please choose an option:\n1-Audio\n2-Video\n3-Quit\n> "))
+url = None
+playlist = None
+
+if opt == 3 or choice == 3:
     exit(1)
+elif opt == 1:
+    url = input("Playlist link: ")
+    playlist = Playlist(url)
+elif opt == 2:
+    url = input("YouTube link: ")
 
 
-def get_audio():
+def rename(title):
+    name = re.sub('[~<>:;"#\'/?*.,|\\\\]', '', title)
+    return name
+
+
+def get_single_audio_file():
+    audio = YouTube(url)
+    try:
+        print(f"Downloading {audio.title}")
+        audio.streams.filter(only_audio=True).get_by_itag(140).download()
+        os.rename(f"{rename(audio.title)}.mp4", f"{rename(audio.title)}.mp3")
+    except AttributeError:
+        print('Attribute error.')
+
+
+def get_single_video_file():
+    video = YouTube(url)
+    print(f"Downloading {video.title}")
+    try:
+        video.streams.filter(file_extension='mp4')
+        print(f"Downloading {video.title}")
+        video.streams.get_highest_resolution().download()
+    except AttributeError:
+        print('Attribute error.')
+
+
+def get_audio_playlist():
     print(f'Downloading: {playlist.title} which has {len(playlist.video_urls)} videos in.')
 
     for audio in playlist.videos:
         try:
             print(f"Downloading {audio.title}")
             audio.streams.filter(only_audio=True).get_by_itag(140).download()
-
-            def rename(title):
-                name = re.sub('[~<>:;"\'/?*.,|\\\\]', '', title)
-                return name
-
             os.rename(f"{rename(audio.title)}.mp4", f"{rename(audio.title)}.mp3")
         except AttributeError:
             print('Attribute error.')
 
 
-def get_video():
+def get_video_playlist():
     print(f'Downloading: {playlist.title} which has {len(playlist.video_urls)} videos in.')
     for video in playlist.videos:
         try:
@@ -38,9 +66,13 @@ def get_video():
 
 
 if __name__ == '__main__':
-    if choice == 1:
-        get_audio()
-    elif choice == 2:
-        get_video()
+    if opt == 1 and choice == 1:
+        get_audio_playlist()
+    elif opt == 1 and choice == 2:
+        get_video_playlist()
+    elif opt == 2 and choice == 1:
+        get_single_audio_file()
+    elif opt == 2 and choice == 2:
+        get_single_video_file()
     else:
         print("Invalid choice.")
